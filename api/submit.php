657c5eb82118016a129154ffac81b5b72f7650cd
mhtgrwl5 <mohitkumaragrawal1590@gmail.com>
2018-03-16 22:30:15 +0530

    LAT AND LONG UPDATE

--- api/submit.php
+++ api/submit.php
@@ -1,5 +1,5 @@
 <?php
-  include 'auth.php';
+  //include 'auth.php';
   $idea=$_POST['title'];
   $des=$_POST['des'];
   $city=$_POST['city'];
@@ -8,14 +8,49 @@
   $temp=$_FILES['img']['tmp_name'];
   $loc_img=uniqid().".jpg";
   if (!move_uploaded_file($temp,$loc_img)) {
-    $ar=array('status'=>'Error');
+    $ar=array('status'=>'Errorimg');
     echo json_encode($ar);
     exit();
   } else{
-  $connect=mysqli_connect('localhost','root','','sih');
-  $query="insert into a_submit values('','Rajan','$idea','$des','$loc_img','21.12221','83.31323','0','$city','$state','$zip')";
-  $result=mysqli_query($connect,$query);
-  if ($result) {
+    $lo="$loc_img";
+    $exif = exif_read_data($lo);
+    if(isset($exif["GPSLatitudeRef"])){
+        $LatM = 1; $LongM = 1;
+        if($exif["GPSLatitudeRef"] == 'S'){
+            $LatM = -1;
+        }
+        if($exif["GPSLongitudeRef"] == 'W'){
+            $LongM = -1;
+        }
+        //get the GPS data
+        $gps['LatDegree']=$exif["GPSLatitude"][0];
+        $gps['LatMinute']=$exif["GPSLatitude"][1];
+        $gps['LatgSeconds']=$exif["GPSLatitude"][2];
+        $gps['LongDegree']=$exif["GPSLongitude"][0];
+        $gps['LongMinute']=$exif["GPSLongitude"][1];
+        $gps['LongSeconds']=$exif["GPSLongitude"][2];
+
+        //convert strings to numbers
+        foreach($gps as $key => $value){
+            $pos = strpos($value, '/');
+            if($pos !== false){
+                $temp = explode('/',$value);
+                $gps[$key] = $temp[0] / $temp[1];
+            }
+        }
+
+    //calculate the decimal degree
+    $result['latitude']  = $LatM * ($gps['LatDegree'] + ($gps['LatMinute'] / 60) + ($gps['LatgSeconds'] / 3600));
+    $result['longitude'] = $LongM * ($gps['LongDegree'] + ($gps['LongMinute'] / 60) + ($gps['LongSeconds'] / 3600));
+    $result['datetime']  = $exif["DateTime"];
+    $lat=$result["latitude"];
+    $long=$result["longitude"];
+    //$result[datetime]
+  }
+  $connect=mysqli_connect('127.0.0.1','root','','sih');
+  $query="insert into a_submit values('','Rajan','$idea','$des','$loc_img','$lat','$long','0','$city','$state','$zip')";
+  $resu=mysqli_query($connect,$query);
+  if ($resu) {
     $ar=array('status' => 'SUCCESS');
     echo json_encode($ar);
   } else {
