01761d27c838fbff23f42b35986c1d12ee7cf427
mhtgrwl5 <mohitkumaragrawal1590@gmail.com>
2018-03-16 16:46:35 +0530

    update

--- sih.sql
+++ sih.sql
@@ -1,22 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 3.3.9
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2018 at 02:49 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.0.26
+-- Generation Time: Mar 16, 2018 at 10:21 AM
+-- Server version: 5.1.53
+-- PHP Version: 5.3.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `sih`
@@ -28,19 +25,19 @@ SET time_zone = "+00:00";
 -- Table structure for table `a_login`
 --
 
-CREATE TABLE `a_login` (
-  `id` int(11) NOT NULL,
-  `username` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
-  `role` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `a_login` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `role` int(1) NOT NULL COMMENT '0:ADMIN 1:USER 2:AUTHORITY',
+  `mobile` text NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `a_login`
 --
 
-INSERT INTO `a_login` (`id`, `username`, `password`, `role`) VALUES
-(1, 'rajan', 'pass', 1);
 
 -- --------------------------------------------------------
 
@@ -48,62 +45,22 @@ INSERT INTO `a_login` (`id`, `username`, `password`, `role`) VALUES
 -- Table structure for table `a_submit`
 --
 
-CREATE TABLE `a_submit` (
-  `userId` int(11) NOT NULL,
-  `idea` int(11) DEFAULT NULL,
-  `description` int(11) DEFAULT NULL,
-  `image` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `longitude` float DEFAULT NULL,
-  `latitude` float DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  `city` int(11) DEFAULT NULL,
-  `state` int(11) DEFAULT NULL,
-  `zip` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `a_submit` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `idea` text NOT NULL,
+  `description` text NOT NULL,
+  `image` text NOT NULL,
+  `longitude` float NOT NULL,
+  `latitude` float NOT NULL,
+  `status` int(11) NOT NULL,
+  `city` text NOT NULL,
+  `state` text NOT NULL,
+  `zip` int(11) NOT NULL,
+  PRIMARY KEY (`post_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `a_submit`
 --
 
-INSERT INTO `a_submit` (`userId`, `idea`, `description`, `image`, `longitude`, `latitude`, `status`, `city`, `state`, `zip`) VALUES
-(1, 0, 0, '0', 21.1222, 83.3132, 0, 0, 0, '416'),
-(2, 0, 0, '5', 21.1222, 83.3132, 0, 0, 0, '416'),
-(3, 0, 0, '5aaa36c3327', 21.1222, 83.3132, 0, 0, 0, '4298'),
-(4, 0, 0, '5aaa3bb1688', 21.1222, 83.3132, 0, 0, 0, '82736847');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `a_login`
---
-ALTER TABLE `a_login`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `a_submit`
---
-ALTER TABLE `a_submit`
-  ADD PRIMARY KEY (`userId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `a_login`
---
-ALTER TABLE `a_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `a_submit`
---
-ALTER TABLE `a_submit`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
