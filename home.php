54bbe8758fff5f7c0963392179f893e0ed4f322f
Rajan <rsahu609@mail.com>
2018-03-17 15:40:19 +0530

    some changes in api newsfeed.php

--- home.php
+++ home.php
@@ -62,7 +62,7 @@
       <div id="map"></div-->
 <!--------------------------- Javascript dependencies  ---------------------------------------------------------------------------------->
     <script src="js/popper.min.js"></script>
-    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/jquery-3.3.1.js"></script>
     <script src="js/bootstrap.bundle.js"></script>
     <!--script src="bootstrap.bundle.js"></script-->
    <script>
