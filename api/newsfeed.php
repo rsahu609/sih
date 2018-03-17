15ca6398c49b995016fa9550696ca925319989a6
mhtgrwl5 <mohitkumaragrawal1590@gmail.com>
2018-03-17 15:39:06 +0530

    newsdatabase update

--- api/newsfeed.php
+++ api/newsfeed.php
@@ -3,7 +3,7 @@
   $connect=mysqli_connect('127.0.0.1','root','','sih');
   $query="SELECT `user_id`, `idea`, `description`, `image`, `latitude`, `longitude`, `status` FROM `a_submit`";
   $result=mysqli_query($connect,$query);
-  $arr = [];
+  $arr;
   while($row = mysqli_fetch_assoc($result)){
       $arr[] = $row;
   }
