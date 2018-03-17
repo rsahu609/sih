02cc85324934dfb463e7322c8b1130cda2ec9558
Asad Siddiqui <msasad8@gmail.com>
2018-03-17 06:38:31 +0530

    Enable CORS

--- api/activity.php
+++ api/activity.php
@@ -1,9 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
 include('../utils.php');
 header('Content-Type: text/json');
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Methods: GET, POST');
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
