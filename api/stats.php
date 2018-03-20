5f7018abe0b7cf4d07ad2367a184809fcd797d07
mhtgrwl5 <mohitkumaragrawal1590@gmail.com>
2018-03-20 16:18:45 +0530

    date time update

--- api/stats.php
+++ api/stats.php
@@ -1,6 +1,28 @@
 <?php
+  $start;
+  $end;
   $connect=mysqli_connect('127.0.0.1','root','password','sih');
-  $query="SELECT COUNT(DISTINCT state) FROM a_submit";
+  if (isset($_REQUEST['year'])) {
+    $year=$_REQUEST['year'];
+    $month=$_REQUEST['month'];
+    if ($month>='1'&&$month<='9') {
+      $start=$year."-0".$month."-01 00:00:01";
+      $end=$year."-0".$month."-31 23:59:59";
+    } else {
+      $start=$year."-".$month."-01 00:00:01";
+      $end=$year."-".$month."-31 23:59:59";
+    }
+    $query="SELECT COUNT(post_id),state FROM a_submit GROUP BY state WHERE  `date` BETWEEN '$start' AND '$end' ";
+
+  } else {
+    $query="SELECT COUNT(post_id), state FROM a_submit GROUP BY state";
+  }
   $count=mysqli_query($connect,$query);
-  print_r($count);
-?>
+  $i=0;
+  $arr;
+  while(($a=mysqli_fetch_assoc($count))){
+    $arr[$i]['state']=$a['state'];
+    $arr[$i]['count']=$a['COUNT(post_id)'];
+  }
+  echo json_encode($arr);
+  ?>
