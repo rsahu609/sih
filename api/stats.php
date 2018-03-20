d84d948a971c26663fa92dae796d0dc2951f9ef5
vibha <vvibha2646@gmail.com>
2018-03-20 14:58:32 +0530

    updates

--- api/stats.php
+++ api/stats.php
@@ -1,6 +1,6 @@
 <?php
-  $connect=mysqli_connect('127.0.0.1','root','password','sih');
+  $connect=mysqli_connect('127.0.0.1','root','','sih');
   $query="SELECT COUNT(DISTINCT state) FROM a_submit";
   $count=mysqli_query($connect,$query);
-  print_r($count);
+  print_r(mysqli_fetch_array($count));
 ?>
