b69536bc23bbc42c9534d34fe0ec6507dc0a080b
Asad Siddiqui <msasad8@gmail.com>
2018-03-22 19:59:40 +0530

    Create stats page

---
+++ js/customchart.js
@@ -0,0 +1,50 @@
+/* global Chart, $*/
+
+$.ajax('api/stats.php', {
+  dataType: 'json'
+}).done(function(response) {
+  const labels = Object.keys(response);
+  const data = [];
+  labels.forEach(function(l) {
+    data.push(response[l]);
+  });
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Number of activities',
+        data: data,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+});
+
+
