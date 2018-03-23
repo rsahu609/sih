00b18036ae54578aa8307551106145663122b1aa
Asad Siddiqui <msasad8@gmail.com>
2018-03-23 22:37:30 +0530

    Add functionality to search in shown map area

--- api/newsfeed.php
+++ api/newsfeed.php
@@ -1,4 +1,47 @@
 <?php
+
+function search_in_lat_long_range($latstart, $latend, $longstart, $longend) {
+  $db = mysqli_connect('localhost', 'root', 'password', 'sih');
+  if(!$db) {
+    error_log('Failed to connect to db');
+    http_response_code(500);
+    exit();
+  }
+
+  $query = "SELECT `post_id`, `user_id`, `idea`, `description`, `image`,".
+    " `city`, `state`, `latitude`, `longitude`, `status` FROM `a_submit` WHERE".
+    " status='1' and latitude between $latstart and $latend and longitude".
+    " between $longstart and $longend";
+
+  $resultset = mysqli_query($db, $query);
+  if(!$resultset) {
+    error_log('Failed to search_in_lat_long_range');
+    http_response_code(500);
+    exit();
+  }
+
+  while($row = mysqli_fetch_assoc($resultset)) {
+    $data[] = $row;
+  }
+
+  return $data;
+}
+
+if($_GET['request_type'] == 'search_in_lat_long_range') {
+
+  $latstart = $_GET['latstart'];
+  $latend = $_GET['latend'];
+  $longstart = $_GET['longstart'];
+  $longend = $_GET['longend'];
+
+  $data = search_in_lat_long_range($latstart, $latend, $longstart, $longend);
+  echo json_encode(array(
+    'activity' => $data,
+  ));
+  exit();
+}
+
+
 if( isset($_REQUEST['request'])){
   include 'connection.php';
   $query = "SELECT `user_id`, `idea`, `description`, `image`, `city`, `state`, `latitude`, `longitude`, `status` FROM `a_submit` WHERE status='1'";
