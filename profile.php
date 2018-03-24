56564ced823577d20d7e5e9f2c5fe0e576b4edac
Rajan <rsahu609@mail.com>
2018-03-24 11:30:14 +0530

    changes in profile form

--- profile.php
+++ profile.php
@@ -14,13 +14,15 @@
     <link href="css/bootstrap.css" rel="stylesheet" id="bootstrap-css">
     <script src="js/bootstrap.bundle.js" ></script>
     <script src="//code.jquery.com/jquery.js"></script>
-    <!------ Include the above in your HEAD tag ---------->
-<div class="image row" >
+    <link rel="stylesheet" href="css/custom.css">
+    <?php include('header.php'); ?>
+
+   <div class="image row" >
     <img src="<?=$_SESSION['profilepic']?>" alt="<?=ucfirst($_SESSION['user'])?>" style="max-height:400px; max-width:4oopx; margin:auto;">
 </div>
     <form id="form">
        
-        <div class="form-row form row" style="max-width:700px;margin:auto;">
+        <div class="form-row form row" style="max-width:700px; margin:auto;">
             <div class="form-group col-md-12">
                 <label for="name">Name</label>
                 <input type="text" class="form-control" name="name" id="name" placeholder="Idea title" autofocus>
@@ -38,9 +40,12 @@
             </div>
             <div class="form-group col-md-12">
                 <label for="procedure">Contacts</label>
-                <input class="form-control" name="number" id="number" placeholder="Enter Your contact details">
+                <input class="form-control" type="number" name="number" id="number" placeholder="Enter Your contact details">
                 <small class="text-muted">Mobile or Telephone number</small>
             </div>
+            
+        <button type="submit" class="btn btn-primary" id="submit">Save Changes</button>
+        <div class="text-success" id="submitstatus" style="display:none;"></div>
         </div>
           
         
@@ -52,5 +57,6 @@
         $('.form-control').on('blur', function() {
             this.closest('.form-group').classList.remove('active');
         });
+    
 </script>
     </body>
