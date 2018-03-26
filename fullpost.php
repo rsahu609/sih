700ac0957e76d0cff43c90304bb1ac3e38cd7a55
mhtgrwl5 <mohitkumaragrawal1590@gmail.com>
2018-03-27 00:41:05 +0530

    done

--- fullpost.php
+++ fullpost.php
@@ -1,6 +1,6 @@
 <?php
 if( isset($_REQUEST['post_id'])){
-  include 'api/connection.php';
+  include 'connection.php';
   $query = "SELECT `user_id`, `idea`, `description`, `image`, `city`,`policy_organization`, `state`, `_procedure`, `policy_details`, `project_budget`, `latitude`,`equipments`, `longitude`, `status`, `upvote` FROM `a_submit` WHERE post_id=$_POST[post_id]";
   $row=mysqli_query($connect,$query);
   $result = mysqli_fetch_assoc($row);
