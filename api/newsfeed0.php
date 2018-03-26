7b87913913859ec5da41d7eb019b5af127cab8a7
mhtgrwl5 <mohitkumaragrawal1590@gmail.com>
2018-03-26 20:50:45 +0530

    again

--- api/newsfeed0.php
+++ api/newsfeed0.php
@@ -2,7 +2,7 @@
 
   include 'connection.php';
   $arr;
-
+  $query;
 if( isset($_REQUEST['request'])){
   $query = "SELECT `user_id`,`post_id`, `idea`, `description`, `image`, `city`, `state`, `latitude`, `longitude`, `status` FROM `a_submit` WHERE status='1'";
   $result=mysqli_query($connect,$query);
@@ -11,16 +11,11 @@ if( isset($_REQUEST['request'])){
   }
   echo json_encode(array('activity'=>$arr));
 } else if( isset($_REQUEST['state'])){
-  $password = 'password';
-  $connect=mysqli_connect('127.0.0.1','root',$password,'sih');
-
   if($_REQUEST['state']=='all'){
     $query="SELECT `user_id`,`post_id`, `idea`, `description`, `image`, `city`, `state`, `latitude`, `longitude`, `status` FROM `a_submit` WHERE status='1' ";
   } else {
     $query="SELECT `user_id`,`post_id`, `idea`, `description`, `image`, `city`, `state`, `latitude`, `longitude`, `status` FROM `a_submit` WHERE status='1' AND `state`='$_REQUEST[state]'";
   }
-
-  $connect=mysqli_connect('127.0.0.1','root',$password,'sih');
   $result=mysqli_query($connect,$query);
 
   while($row = mysqli_fetch_assoc($result)){
